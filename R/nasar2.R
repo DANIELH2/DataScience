fedae727da8add8405af419cad44d3ac08a9b0e1
danidin1234@gmail.com <danidin1234@gmail.com>
2019-11-14 14:53:53 +0200

    update

---
+++ R/nasar2.R
@@ -0,0 +1,15 @@
+library(dplyr)
+nasa1 <- as_data_frame(nasa)
+
+nasa2 <- nasa1 %>%
+  filter(lat >= 29.56 & lat <= 33.09 & long <= -90.55 & long >= -110.93) %>%
+  mutate(tempsurf_ratio=temperature/surftemp) %>%
+  group_by(year) %>%
+  summarise(pressure_mean=mean(pressure,na.rm = T),
+            pressure_sd=sd(pressure,na.rm = T),
+            ozone_mean=mean(ozone,na.rm=T),
+            ozone_sd=sd(ozone,na.rm = T),
+            tempsurf_ratio_mean=mean(tempsurf_ratio,na.rm = T),
+            tempsurf_ratio_sd=sd(tempsurf_ratio,na.rm=T)) %>%
+  arrange(desc(ozone_mean))
+
